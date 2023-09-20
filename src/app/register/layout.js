e573e33fd3397bac9558ecf2995327e39e61b2dd
David Abolade <69032300+A-believer@users.noreply.github.com>
2023-09-21 00:04:43 +0100

    intial setup

---
+++ src/app/register/layout.js
@@ -0,0 +1,9 @@
+
+
+export default function Layout({ children }) {
+  return (
+    
+      <section className="text-6xl">{children}</section>
+   
+  )
+}
---
+++ src/app/register/page.js
@@ -0,0 +1,11 @@
+import FirstComponent from '@/components/FirstComponent'
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div>
+      Register
+      <FirstComponent/>
+    </div>
+  )
+}
